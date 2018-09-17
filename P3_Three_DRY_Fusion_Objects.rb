4be67a464ac8839cbf46aa9c90a5f7bd10fd8752
Christian Torres <cctorresr@unal.edu.co>
2018-09-16 20:35:00 -0500

    Ajuste en salida

--- P3_Three_DRY_Fusion_Objects.rb
+++ P3_Three_DRY_Fusion_Objects.rb
@@ -8,7 +8,7 @@ class Archivo
   def lineas
     arrayLineas = Array.new
     File.open( archivo, 'r') do |f1|
-      f1.gets #Descarto linea de encabezados, porque no me interesa
+      f1.gets #Descarto linea de encabezados
       while linea = f1.gets
         arrayLineas << linea if linea.strip[0] != "-" && !linea.strip.empty?
       end
@@ -18,10 +18,9 @@ class Archivo
 end
 
 class Procesador
-  def encontrarMenor array, posValor1, posValor2, posValorARetornar
-    linea1 = array[0].split(" ")
-    array.shift
-    menor = linea1[posValorARetornar]
+  def encontrarMenor array, posValor1, posValor2, posIndiceARetornar
+    linea1 = array.shift.split(" ")
+    menor = linea1[posIndiceARetornar]
     valorMenor = (linea1[posValor1].to_f - linea1[posValor2].to_f).abs
 
     array.each do |linea|
@@ -29,7 +28,7 @@ class Procesador
       resta = (datos[posValor1].to_f - datos[posValor2].to_f).abs
       if resta < valorMenor
         valorMenor = resta
-        menor = datos[posValorARetornar]
+        menor = datos[posIndiceARetornar]
       end
     end
     menor
@@ -38,6 +37,8 @@ end
 
 archivo = Archivo.new('weather.dat')
 procesador = Procesador.new()
-puts procesador.encontrarMenor archivo.lineas, 1, 2, 0
+menor = procesador.encontrarMenor archivo.lineas, 1, 2, 0
+puts "El día con la menor diferencia de temperatura es el #{menor}"
 archivo.archivo = 'football.txt'
-puts procesador.encontrarMenor archivo.lineas, 6, 8, 1
+menor = procesador.encontrarMenor archivo.lineas, 6, 8, 1
+puts "El equipo con la menor diferencia entre goles a favor y en contra es el #{menor}"
